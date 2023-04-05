2e846401b2c964440b510e6e7d0bf35b3c0fccca
Fónad Gergely <fonad.gergely@stud.uni-obuda.hu>
2023-04-05 19:36:54 +0200

    asd

---
+++ QCIFSA_HFT_2022232.Repository/SeriesDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QCIFSA_HFT_2022232.Repository
+{
+    internal class SeriesDbContext
+    {
+    }
+}
